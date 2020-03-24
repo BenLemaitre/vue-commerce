4fad20d033072c71794769ab48874f2995d39d07
Ben <benjamin.lemaitre@yandex.com>
2020-03-25 00:05:08 +0100

    working API

---
+++ server/routes/owner.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+const Owner = require("../models/owner");
+
+const upload = require("../middlewares/upload-photo");
+
+router.post("/owners", upload.single("photo"), async (req, res) => {
+  try {
+    const owner = new Owner();
+    owner.name = req.body.name;
+    owner.about = req.body.about;
+    owner.photo = req.file.location;
+
+    await owner.save();
+
+    res.json({
+      success: true,
+      message: "saved!"
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
+router.get("/owners", async (req, res) => {
+  try {
+    let owners = await Owner.find();
+    res.json({
+      success: true,
+      owners
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,22 +1,40 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+
+const PORT = process.env.PORT;
+
+mongoose.connect(
+  process.env.DATABASE,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to DB");
+    }
+  }
+);
 
 // Middlewares
 app.use(morgan("dev")); // logs http requests
 app.use(bodyParser.json()); // parse data to proper format
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.json("Hello app");
-});
+// Required APIs
+const productRoutes = require("./routes/product");
+const categoryRoutes = require("./routes/category");
+const ownerRoutes = require("./routes/owner");
 
-app.post("/", (req, res) => {
-  console.log(req.body.name);
-});
+app.use("/api", productRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", ownerRoutes);
 
 app.listen(PORT, err => {
   if (err) {
