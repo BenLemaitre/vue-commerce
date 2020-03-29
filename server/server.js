0db31f0155b353b7ef79f80369344afdf6bf93a5
Ben <benjamin.lemaitre@yandex.com>
2020-03-29 18:56:19 +0200

    backend - user authentication

--- server/server.js
+++ server/server.js
@@ -33,10 +33,12 @@ app.use(cors());
 const productRoutes = require("./routes/product");
 const categoryRoutes = require("./routes/category");
 const ownerRoutes = require("./routes/owner");
+const userRoutes = require("./routes/auth");
 
 app.use("/api", productRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", ownerRoutes);
+app.use("/api", userRoutes);
 
 app.listen(PORT, err => {
   if (err) {
